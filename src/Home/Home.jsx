a08deeee05d0125279e94c6f5d15d4725e219563
DrabinPaul <drabinpaul1429@gmail.com>
2023-09-14 20:49:10 +0600

    setup react project with tailwind CSS

---
+++ src/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-4xl font-bold text-center'>Course Registration</h1>
+        </div>
+    );
+};
+
+export default Home;
